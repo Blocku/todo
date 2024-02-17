68853437237dca0a08a9fcc88d854e4ea5eae5ff
Blocku b <urent111@yandex.ru>
2024-02-17 11:03:58 +0000

    1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue
+      }
+    },
+  },
+  plugins: [],
+}
+
