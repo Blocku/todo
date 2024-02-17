b2d3a186eec56de9f5386a77ddb2e0e72173a2ba
Blocku b <urent111@yandex.ru>
2024-02-17 10:40:52 +0000

    1

--- todo/src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div class="h-full w-full sm:h-auto sm:w-auto p-5 bg-white border-2 border-zinc-300 shadow-xl rounded-xl space-y-5">
-    <div class="w-full h-auto flex gap-3 justify-between">
-        <input class="flex-grow break-words p-3 rounded-lg ring-2 ring-zinc-300 focus:outline-none focus:ring-2 focus:ring-zinc-400 " type="text" placeholder="text">
-        <button class="btn btn--primary">Add</button>
-    </div>
-</div>
