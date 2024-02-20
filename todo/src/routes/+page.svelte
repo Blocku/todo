5bf9beade0936c7892ad1599f921111960ba760f
Blocku b <urent111@yandex.ru>
2024-02-20 15:42:44 +0000

    2

--- todo/src/routes/+page.svelte
+++ todo/src/routes/+page.svelte
@@ -1,6 +1,8 @@
-<div class="h-full w-full sm:h-auto sm:w-auto p-5 bg-white border-2 border-zinc-300 shadow-xl rounded-xl space-y-5">
-    <div class="w-full h-auto flex gap-3 justify-between">
-        <input class="flex-grow break-words p-3 rounded-lg ring-2 ring-zinc-300 focus:outline-none focus:ring-2 focus:ring-zinc-400 " type="text" placeholder="text">
+<div class="w-full h-full border-2 border-zinc-300 rounded-xl shadow-xl px-3 py-5 space-y-7">
+
+    <div class="w-full h-auto flex justify-between gap-3 ">
+        <input class="h-auto w-[70%] py-2.5 px-3 rounded-lg focus:outline-none ring-2 ring-zinc-300 focus:ring-zinc-400 " type="text" placeholder="text" >
         <button class="btn btn--primary">Add</button>
     </div>
+
 </div>
