eac19cb1f77763f04ce4b21f9d0321b7f5fb5232
Blocku b <urent111@yandex.ru>
2024-02-21 15:31:12 +0000

    3

--- todo/src/routes/+page.svelte
+++ todo/src/routes/+page.svelte
@@ -1,15 +1,14 @@
-<div class="w-full md:w-[600px] xl:w-[650px] h-full md:h-[70dvh] border-2 border-secondary-300 rounded-xl shadow-xl px-3 pt-5 pb-24 space-y-7 ">
+<div class="w-full md:w-[500px] h-full md:h-[70dvh] md:border-2 md:border-secondary-300 md:rounded-xl md:shadow-xl px-3 pt-3 pb-[5.5rem] space-y-7 ">
 
     <div class="w-full h-auto flex justify-between gap-3 ">
         <input class="h-auto w-[70%] py-2.5 px-3 rounded-lg focus:outline-none ring-2 ring-secondary-300 focus:ring-secondary-400 " type="text" placeholder="text" >
-        <button class="btn btn--primary">Add</button>
+        <button on:click={buttonClick} type='button' class="btn btn--primary">Add</button>
     </div>
-    <ul class="w-full h-full space-y-7 overflow-auto">
-        <li class="w-full h-auto py-1 px-1 flex justify-between items-center gap-3 border-b-2 border-secondary-300">
+    <ul id='table' class="w-full h-full space-y-7 overflow-auto">
+        {#each list as item}<li class="w-full h-auto py-1 px-1 flex justify-between items-center gap-3 border-b-2 border-secondary-300">
             <p class="w-full h-auto text-base font-medium break-all ">Get up</p>
-            <TrashIcon class="h-6 w-6 fill-red-500 hover:cursor-pointer"/>      
-        </li>
-
+            <TrashIcon id="trash" class="h-6 w-6 fill-red-500 hover:cursor-pointer"/>      
+        </li>{/each}
     </ul>
 </div>
 
@@ -17,4 +16,12 @@
 
 <script>
 	import { TrashIcon } from 'heroicons-svelte/24/solid';
+
+    let list = ["you are lox"]
+
+
+    function buttonClick()(
+        list.push("you are lox")
+    )
+
 </script>
