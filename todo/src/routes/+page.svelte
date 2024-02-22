a58380a1a7fa8cd441376c89b97402a6c09d8134
Blocku b <urent111@yandex.ru>
2024-02-22 16:48:09 +0000

    4

--- todo/src/routes/+page.svelte
+++ todo/src/routes/+page.svelte
@@ -2,13 +2,13 @@
 
     <div class="w-full h-auto flex justify-between gap-3 ">
         <input class="h-auto w-[70%] py-2.5 px-3 rounded-lg focus:outline-none ring-2 ring-secondary-300 focus:ring-secondary-400 " type="text" placeholder="text" >
-        <button on:click={buttonClick} type='button' class="btn btn--primary">Add</button>
+        <button type='button' class="btn btn--primary">Add</button>
     </div>
     <ul id='table' class="w-full h-full space-y-7 overflow-auto">
-        {#each list as item}<li class="w-full h-auto py-1 px-1 flex justify-between items-center gap-3 border-b-2 border-secondary-300">
+        <li class="w-full h-auto py-1 px-1 flex justify-between items-center gap-3 border-b-2 border-secondary-300">
             <p class="w-full h-auto text-base font-medium break-all ">Get up</p>
             <TrashIcon id="trash" class="h-6 w-6 fill-red-500 hover:cursor-pointer"/>      
-        </li>{/each}
+        </li>
     </ul>
 </div>
 
@@ -17,11 +17,6 @@
 <script>
 	import { TrashIcon } from 'heroicons-svelte/24/solid';
 
-    let list = ["you are lox"]
-
-
-    function buttonClick()(
-        list.push("you are lox")
-    )
+    
 
 </script>
